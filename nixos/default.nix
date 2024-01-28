f2ccc8c82a0e8014201aa2f0073a07c56a86c014
Tracteur Blindé <tracteur@chiral.dev>
2024-01-27 17:52:14 -0800

    Support for OS arch override

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,21 +1,40 @@
 { nixpkgs, lanzaboote, dotfiles-private, dotfiles-utils, ... }@inputs:
 let
-  mkOSConfig = host: nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [
-      ./common.nix
-      ./users.nix
-      ./hosts/${host}.nix
-      dotfiles-private.hardware.${host}
-      dotfiles-private.nixosCommon
+  lib = nixpkgs.lib;
+  mkOSConfig = host:
+    let
+      system = "x86_64-linux";
+      hostModule = dotfiles-private.hardware.${host};
+      platformConfig = { config, ... } : {
+        nixpkgs.hostPlatform = {
+          inherit system;
+          gcc = lib.optionalAttrs (hostModule ? gcc) {
+            inherit (hostModule.gcc) arch tune;
+          };
+        };
+        # Needed when transitioning from a generic x86_64-linux system to a specialized one
+        # nix.settings.system-features = lib.optionalAttrs (hostModule ? gcc) [ "nixos-test" "benchmark" "big-parallel" "kvm" "gccarch-${hostModule.gcc.arch}" ];
+      };
+    in
+    nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./common.nix
+        ./users.nix
+        ./hosts/${host}.nix
+        hostModule.config
+        dotfiles-private.nixosCommon
+        platformConfig
 
-      lanzaboote.nixosModules.lanzaboote
-      dotfiles-utils.unfreeMerger
-    ];
+        lanzaboote.nixosModules.lanzaboote
+        dotfiles-utils.unfreeMerger
+      ];
 
-    # Make flake inputs and dotfiles-utils available in modules.
-    specialArgs = { inherit inputs dotfiles-private dotfiles-utils; };
-  };
+      # Make flake inputs and dotfiles-utils available in modules.
+      specialArgs = {
+        inherit inputs dotfiles-private dotfiles-utils;
+      };
+    };
 
   hosts = dotfiles-utils.findNixFilesInDir ./hosts;
 in
