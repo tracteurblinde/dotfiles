9e20f9964bf7cd03d22b99382bbbc3366b704705
Tracteur Blindé <tracteur@chiral.dev>
2024-02-23 19:33:30 -0800

    + cura

--- home/home.nix
+++ home/home.nix
@@ -8,6 +8,32 @@ let
       sha256 = "0v5vwr8s11pq0dz35mwgydncf05wc07psg506q61z2fq9i99kkg2";
     };
   });
+
+  # nixpkgs only exposes cura 4 due to difficulties with the cura 5 build system
+  # Just use the appimage for now
+  # From @MarSoft https://github.com/NixOS/nixpkgs/issues/186570#issuecomment-1627797219
+  cura5 = (let cura5 = pkgs.appimageTools.wrapType2 rec {
+      name = "cura5";
+      version = "5.6.0";
+      src = pkgs.fetchurl {
+        url = "https://github.com/Ultimaker/Cura/releases/download/${version}/UltiMaker-Cura-${version}-linux-X64.AppImage";
+        hash = "sha256-EHiWoNpLKHPzv6rZrtNgEr7y//iVcRYeV/TaCn8QpEA=";
+      };
+      extraPkgs = pkgs: with pkgs; [ ];
+    }; in pkgs.writeScriptBin "cura" ''
+      #! ${pkgs.bash}/bin/bash
+      # AppImage version of Cura loses current working directory and treats all paths relative to $HOME.
+      # So we convert each of the files passed as argument to an absolute path.
+      # This fixes use cases like `cd /path/to/my/files; cura my_model.stl another_model.stl`.
+      args=()
+      for a in "$@"; do
+        if [ -e "$a" ]; then
+          a="$(realpath "$a")"
+        fi
+        args+=("$a")
+      done
+      exec "${cura5}/bin/cura5" "''${args[@]}"
+    '');
 in
 {
   imports = [
@@ -52,6 +78,7 @@ in
     tmux
 
     # Productivity Suite
+    cura5
     chromium
     libreoffice
     fluent-reader
