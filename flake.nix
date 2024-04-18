6e40ef189d563ec0c1578137d7f51a9ee465cd6e
Tracteur Blindé <tracteur@chiral.dev>
2024-04-18 15:42:28 -0700

    Remove nixos-hardware redirect (PR has been merged)

--- flake.nix
+++ flake.nix
@@ -33,9 +33,7 @@
     # NixOS Modules
     ##
     # Surface Patches
-    # Overriding awaiting https://github.com/NixOS/nixos-hardware/pull/878
-    # nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixos-hardware.url = "github:tracteurblinde/nixos-hardware/surface-linux-6.8.1";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     # Used for Secure Boot
     lanzaboote = {
