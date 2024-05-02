77e0e01ccc67405592c257f7734ea37b039241fb
Tracteur Blindé <tracteur@chiral.dev>
2024-05-02 12:13:34 -0700

    Test community cache

--- flake.nix
+++ flake.nix
@@ -70,14 +70,16 @@
     # };
   };
 
-  # nixConfig = {
-  #   extra-substituters = [
-  #     "https://hyprland.cachix.org"
-  #   ];
-  #   extra-trusted-public-keys = [
-  #     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-  #   ];
-  # };
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      # "https://hyprland.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
 
   outputs = inputs@{ nixos-unstable, nixpkgs-unstable, ... }:
     let
