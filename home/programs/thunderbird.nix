100902d2b74d2405fa2e4deeb4391b36e735f46d
Tracteur Blindé <tracteur@chiral.dev>
2024-10-25 16:22:49 -0700

    [thunderbird] WIP

--- home/programs/thunderbird.nix
+++ home/programs/thunderbird.nix
@@ -1,5 +1,44 @@
 { pkgs, ... }:
 let
+  # ```
+  # diff -u ~/.config/mimeapps.list*                                                                                                                                                                        ✔ 
+  # --- ~/.config/mimeapps.list    1969-12-31 16:00:01.000000000 -0800
+  # +++ ~/.config/mimeapps.list.backup     2024-09-10 14:51:25.571880674 -0700
+  # @@ -1,6 +1,16 @@
+  #  [Added Associations]
+  #  x-scheme-handler/sms=org.gnome.Shell.Extensions.GSConnect.desktop
+  #  x-scheme-handler/tel=org.gnome.Shell.Extensions.GSConnect.desktop
+  # +x-scheme-handler/mailto=userapp-Thunderbird-IKVZT2.desktop;
+  # +x-scheme-handler/mid=userapp-Thunderbird-IKVZT2.desktop;
+  # +x-scheme-handler/news=userapp-Thunderbird-LKC2T2.desktop;
+  # +x-scheme-handler/snews=userapp-Thunderbird-LKC2T2.desktop;
+  # +x-scheme-handler/nntp=userapp-Thunderbird-LKC2T2.desktop;
+  # +x-scheme-handler/feed=userapp-Thunderbird-11S3T2.desktop;
+  # +application/rss+xml=userapp-Thunderbird-11S3T2.desktop;
+  # +application/x-extension-rss=userapp-Thunderbird-11S3T2.desktop;
+  # +x-scheme-handler/webcal=userapp-Thunderbird-TS73T2.desktop;
+  # +x-scheme-handler/webcals=userapp-Thunderbird-TS73T2.desktop;
+
+  #  [Default Applications]
+  #  application/x-extension-htm=librewolf.desktop
+  # @@ -25,5 +35,18 @@
+  #  x-scheme-handler/tel=org.gnome.Shell.Extensions.GSConnect.desktop
+  #  x-scheme-handler/tidal=tidal-hifi.desktop
+  #  x-scheme-handler/unknown=librewolf.desktop
+  # +x-scheme-handler/mailto=userapp-Thunderbird-IKVZT2.desktop
+  # +message/rfc822=userapp-Thunderbird-IKVZT2.desktop
+  # +x-scheme-handler/mid=userapp-Thunderbird-IKVZT2.desktop
+  # +x-scheme-handler/news=userapp-Thunderbird-LKC2T2.desktop
+  # +x-scheme-handler/snews=userapp-Thunderbird-LKC2T2.desktop
+  # +x-scheme-handler/nntp=userapp-Thunderbird-LKC2T2.desktop
+  # +x-scheme-handler/feed=userapp-Thunderbird-11S3T2.desktop
+  # +application/rss+xml=userapp-Thunderbird-11S3T2.desktop
+  # +application/x-extension-rss=userapp-Thunderbird-11S3T2.desktop
+  # +x-scheme-handler/webcal=userapp-Thunderbird-TS73T2.desktop
+  # +text/calendar=userapp-Thunderbird-TS73T2.desktop
+  # +application/x-extension-ics=userapp-Thunderbird-TS73T2.desktop
+  # +x-scheme-handler/webcals=userapp-Thunderbird-TS73T2.desktop
+  # ```
   mimeTypes = [
     "x-scheme-handler/mailto"
     "message/rfc822"
@@ -19,15 +58,17 @@ in
     mimeTypes);
 
   home.packages = with pkgs; [
-    thunderbird
+    #thunderbird
     birdtray
     protonmail-bridge-gui
   ];
 
   # TODO: Use HM to configure this!
-  #   Accounts are a requirement for using home-manager to manage Thunderbird
-  #   We haven't migrated that in yet.
   programs.thunderbird = {
-    # enable = true;
+    enable = true;
+    profiles.default = {
+      isDefault = true;
+
+    };
   };
 }
